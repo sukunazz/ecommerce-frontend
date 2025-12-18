83da31850ce5235e82e19d3693fbe6f0c8795c7d
sukunazz <sujansigdel03@gmail.com>
2025-12-18 20:09:42 +0545

    api url fixed

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_BASE_URL = process.env.API_URL;
 
 type ApiFetchOptions = RequestInit & {
   skipAuthRedirect?: boolean;
