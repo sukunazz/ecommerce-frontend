7389d237e0eb6a2efd70e1f97aa40cbbc7d72f60
sukunazz <sujansigdel03@gmail.com>
2025-12-24 16:01:18 +0545

    cartpage

--- app/(protected)/cart/page.tsx
+++ app/(protected)/cart/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { useCart } from "@/hooks/cart/useCart";
+import { useCart } from "@/context/cart/CartContext";
 import { apiFetch } from "@/lib/api";
 import { useRouter } from "next/navigation";
 
